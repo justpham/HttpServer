cf9137c3d6d15e327a0352192b31b2b3427ae201
justpham <jp122711@gmail.com>
2025-08-25 13:22:09 -0700

    Added basic server router

--- src/server/server.c
+++ src/server/server.c
@@ -23,66 +23,101 @@
 #include <unistd.h>
 
 int
+server_router (HTTP_MESSAGE *request, HTTP_MESSAGE *response)
+{
+
+    char *route = request->start_line.request.request_target;
+    char *method = request->start_line.request.method;
+
+    strcpy(response->start_line.response.protocol, request->start_line.request.protocol);
+
+    if (strcmp(route, "/") == 0) {
+
+        if (strcmp(method, "GET") == 0) {
+
+            strcpy(response->start_line.response.status_code, "200");
+            strcpy(response->start_line.response.status_message, "OK");
+
+            response->header_count = 4;
+
+            response->headers[0] = (HTTP_HEADER){ "Server", "HttpServer" };
+            response->headers[1] = (HTTP_HEADER){ "Content-Type", "text/html; charset=UTF-8" };
+            response->headers[2] = (HTTP_HEADER){ "Content-Length", "228" };
+            response->headers[3] = (HTTP_HEADER){ "Connection", "close" };
+
+            int html_file = open("html/index.html", O_RDONLY);
+
+            http_message_set_body_fd(response, html_file, NULL, 228);
+
+        } else {
+            strcpy(response->start_line.response.status_code, "405");
+            strcpy(response->start_line.response.status_message, "Method Not Allowed");
+
+            response->header_count = 4;
+            response->headers[0] = (HTTP_HEADER){ "Server", "HttpServer" };
+            response->headers[1] = (HTTP_HEADER){ "Content-Length", "228" };
+            response->headers[2] = (HTTP_HEADER){ "Allow", "GET" };
+            response->headers[3] = (HTTP_HEADER){ "Connection", "close" };
+        }
+
+    } else {
+        strcpy(response->start_line.response.status_code, "404");
+        strcpy(response->start_line.response.status_message, "Not Found");
+
+        response->header_count = 4;
+        response->headers[0] = (HTTP_HEADER){ "Server", "HttpServer" };
+        response->headers[1] = (HTTP_HEADER){ "Content-Type", "text/html; charset=UTF-8" };
+        response->headers[2] = (HTTP_HEADER){ "Content-Length", "240" };
+        response->headers[3] = (HTTP_HEADER){ "Connection", "close" };
+
+        int html_file = open("html/NotFound.html", O_RDONLY);
+
+        http_message_set_body_fd(response, html_file, NULL, 240);
+
+    }
+
+    return 0;
+}
+
+
+
+int
 main(void)
 {
     // listen on sock_fd, new connection on new_fd
-    int sockfd, new_fd;
+    int sockfd, client_fd;
 
     sockfd = server_setup();
 
     while (1) { // main accept() loop
-        if ((new_fd = accept_connection(sockfd)) == -1) {
+        if ((client_fd = accept_connection(sockfd)) == -1) {
             continue;
         }
 
         if (!fork()) {
             close(sockfd); // child doesn't need this
 
+            HTTP_MESSAGE response = init_http_message();
+
             // Recieve HTTP Request
-            HTTP_MESSAGE request = parse_http_message(new_fd, REQUEST);
+            HTTP_MESSAGE request = parse_http_message(client_fd, REQUEST);
 
             // Print out the request info for debug purposes
             print_http_message(&request);
 
-            // Handle HTTP Headers
+            // Put Request in Router
+            server_router(&request, &response);
 
-            // Handle HTTP Body
-
-            // Create HTTP Response
-            HTTP_MESSAGE response = init_http_message();
-
-            printf("Response:\n");
-
-            strcpy(response.start_line.response.protocol, request.start_line.request.protocol);
-            strcpy(response.start_line.response.status_code, "200");
-            strcpy(response.start_line.response.status_message, "OK");
-
-            printf("Response:\n");
-            print_http_message(&response);
-
-            response.header_count = 4;
-            response.headers[0] = (HTTP_HEADER){ "Server", "HttpServer" };
-            response.headers[1] = (HTTP_HEADER){ "Content-Type", "text/html; charset=UTF-8" };
-            response.headers[2] = (HTTP_HEADER){ "Content-Length", "228" };
-            response.headers[3] = (HTTP_HEADER){ "Connection", "close" };
-
-            int html_file = open("html/index.html", O_RDONLY);
-
-            http_message_set_body_fd(&response, html_file, NULL, 228);
-
-            print_http_message(&response);
-
-            // Send HTTP Response
-            build_and_send_message(&response, new_fd, RESPONSE);
+            build_and_send_message(&response, client_fd, RESPONSE);
 
             // Clean everything up
             free_http_message(&request);
-            close(new_fd);
+            close(client_fd);
 
             exit(0);
         }
 
-        close(new_fd); // parent doesn't need this
+        close(client_fd); // parent doesn't need this
     }
 
     return 0;
